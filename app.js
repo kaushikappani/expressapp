2744651947f08ea1fa4785e198f3361477ffc8b2
Appani Kaushik <58048398+kaushikappani@users.noreply.github.com>
2021-02-10 13:41:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
-    console.log('Server started')
+    console.log(`server started in port ${port}`)
 })
